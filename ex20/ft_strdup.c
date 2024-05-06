a6ff6de29329dce590b202921d1972a44fd58cfc
Gerson Carvalho <gecarval@c1r10s7.42luanda.com>
2024-05-06 17:48:09 +0100

    version 0.4

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gecarval <gecarval@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 17:42:22 by gecarval          #+#    #+#             */
+/*   Updated: 2024/05/06 17:48:04 by gecarval         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*dup;
+
+	i = 0;
+	while (src[i])
+		i++;
+	dup = (int *)malloc(sizeof(int) * (i + 1));
+	if (!dup)
+		return (NULL);
+	i = -1;
+	while (src[++i])
+		dup[i] = src[i];
+	dup[i] = '\0';
+	return (dup);
+}
